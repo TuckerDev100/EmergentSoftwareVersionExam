381e94a1ae094f7c9591201dee675de1d4e0385e
tuckerdev <tuckerdev100@outlook.com>
2023-08-03 13:46:17 -0600

    cleanup of Version exam

--- VersionAssesment.js
+++ VersionAssesment.js
@@ -38,7 +38,7 @@ function VersionCompare(version1, version2) {
         loop = 0;
         for (let i = 1; i <= ver2.length; i++){
             if (ver1[i] == null){
-                ver1[i] = 0
+                ver1[i] = 0;
             }
             if (ver2[i] == ver1[i]) {
                 continue;
@@ -54,80 +54,4 @@ function VersionCompare(version1, version2) {
     }
 }
 
- console.log(VersionCompare("2.0.0.0.1", "2"))
-//problem: ("2", "2.0.0.0.0") 0, ("2", "2.0.0.0.1") -1, ("2.0.0.0.0", ""2"") 0, ("2.0.0.0.1", "2" 1)
-
-
-
-
-
-
-
-// function VersionCompare(version1, version2) {
-//     let verArr1 = version1.split(".");
-//     let verArr2 = version2.split(".");
-
-//     for (let i = 0; i <= verArr1.length; i++) {
-//         if (verArr1[i] < verArr2[i]) {
-//             console.log()
-//             return(-1);
-//         }
-//         else if (verArr1[i] > verArr2[i]) {
-//             return(1);
-//         }
-//         else if (verArr1[i] == verArr2[i]) {
-
-//             return 0;
-//             continue;
-//         }
-
-//     }
-
-//     if (verArr1.length < verArr2.length) {
-//         for (let g = verArr1.length; g <= verArr2.length; g++) {
-//             if (verArr2[g] == 0) {
-//                 continue;
-//             } else if (verArr2[g] > 0) {
-//                 return (-1);
-//             }
-//             }
-//         }
-    
-
-//     if (verArr1.length > verArr2.length) {
-//         for (let k = verArr2.length; k <= verArr1.length; k++) {
-//             if (verArr2[k] == 0) {
-//                 continue;
-//             }
-//             else if (verArr2[k] > 0) {return(-1) }
-//         }
-//     }
-//     return (0)
-// }
-
-
-// VersionCompare("2", "2.1")
-
-// console.log(VersionCompare("2", "2.1"))
-
-//problem: ("2", "2.0.0.0.0") 0, ("2", "2.0.0.0.1") -1, ("2.0.0.0.0", ""2"") 0, ("2.0.0.0.1", "2" 1)
-
-// function VersionCompare(version1, version2) {
-//     let ver1 = version1.split(".");
-//     let ver2 = version2.split(".");
-
-//     let longer
-//     let shorter 
-//     let same
-
-//     if (ver1.length > ver2.length) {
-//         ver1 = longer
-//         ver2 = shorter
-//     } else if (ver1.lenth < ver2.length) {
-//         ver1 = shorter
-//         ver2 = longer
-//     }
-
-//     for (let i = shorter.length; i <= longer.length; i++) {
-        
-//     }
+ console.log(VersionCompare("2.0.0.0.1", "2"));
